f7e57e44871bd5d166bc3ae6858c66caf383f419
Fernando Ferreira Silva <fernando@ferreirasil.com>
2019-04-28 00:19:36 -0300

    enum for widgets

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include "componentview.h"
 #include "supplierview.h"
 #include <QLabel>
+#include <QDebug>
+#include <QMessageBox>
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -10,10 +12,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //SupplierView *sup = new SupplierView;
-    //sup->show();
-    ComponentView *comp = new ComponentView;
-    comp->show();
+    createChildWidgets();
+
 }
 
 MainWindow::~MainWindow()
@@ -29,3 +29,47 @@ void MainWindow::validUser(QString user)
 
 
 
+void MainWindow::createChildWidgets()
+{
+    widgetsViews.at(static_cast<int>(widgetId::SUPPLIER_VIEW)) = new SupplierView(this->ui->widgetView);
+    widgetsViews.at(static_cast<int>(widgetId::PART_VIEW)) = new ComponentView(this->ui->widgetView);
+    hideAllChildWidgets();
+}
+
+void MainWindow::hideAllChildWidgets()
+{
+    for(auto i : widgetsViews){
+        i->hide();
+    }
+}
+
+void MainWindow::hideLastChildWidget()
+{
+    if(currentWidget != widgetId::NULL_WIDGET){
+        widgetsViews.at(static_cast<unsigned long>(currentWidget))->hide();
+    }
+}
+
+void MainWindow::showChildWidget(MainWindow::widgetId id)
+{
+    hideLastChildWidget();
+    widgetsViews.at(static_cast<unsigned long>(id))->show();
+    currentWidget = id;
+}
+
+void MainWindow::on_actionRegisterSupplier_triggered()
+{
+    showChildWidget(widgetId::SUPPLIER_VIEW);
+}
+
+void MainWindow::on_actionRegisterPart_triggered()
+{
+    showChildWidget(widgetId::PART_VIEW);
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    if(QMessageBox::question(this,"Sair","Deseja sair?","não","sim")){
+        QApplication::quit();
+    }
+}
